c826d7e78bf04dd478ddb842814dcacf3235cc1c
Najib Ishaq <najib_ishaq@zoho.com>
2022-04-11 15:58:24 -0400

    added requirements

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import keract
-import numpy as np
+import numpy
 from keras import layers
 from tensorflow import keras
 
@@ -18,8 +18,8 @@ def get_mnist_data(is_model_conv: bool):
 
     if is_model_conv:
         # Make sure images have shape (28, 28, 1)
-        x_train = np.expand_dims(x_train, -1)
-        x_test = np.expand_dims(x_test, -1)
+        x_train = numpy.expand_dims(x_train, -1)
+        x_test = numpy.expand_dims(x_test, -1)
     print("x_train shape:", x_train.shape)
     print(x_train.shape[0], "train samples")
     print(x_test.shape[0], "test samples")
